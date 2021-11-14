5edd2bede1731c447c47cd7fec27204f8199c517
ivanov sergey <ivanov@ivanov-media.ru>
2021-11-14 16:39:46 +0300

    Домашнее задание 4 - квадратный корень

---
+++ src/main/java/Lesson4/Lesson4_1.java
@@ -0,0 +1,10 @@
+package Lesson4;
+// Чтобы извлечь квадратный корень из числа, применяют метод sqrt.
+
+public class Lesson4_1 {
+    public static void main(String[] args) {
+        int с = 25;
+        double b = Math.sqrt(с);
+        System.out.println("Корень квадратный " + с + " = " + b);
+    }
+}
